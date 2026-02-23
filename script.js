2e5d205bed781fd902b561ea597e8ced2221e9bd
SabirMCoadCraftier <mahmudcse24@gmail.com>
2026-02-24 03:47:46 +0600

    Memmory in job data

---
+++ script.js
@@ -0,0 +1,34 @@
+// memory job data
+const jobs = [
+  { 
+    id: 1, 
+    status: "not_applied" 
+  },
+  { id: 2, 
+    status: "not_applied" 
+  },
+  { 
+    id: 3, 
+    status: "not_applied" 
+  },
+  { 
+    id: 4,
+    status: "not_applied" 
+  },
+  { 
+    id: 5, 
+    status: "not_applied" 
+  },
+  { 
+    id: 6, 
+    status: "not_applied" 
+  },
+  { id: 7, 
+    status: "not_applied" 
+  },
+  { id: 8, 
+    status: "not_applied" 
+  }
+];
+
+document.addEventListener("DOMContentLoaded", function () {
