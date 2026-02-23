c616341daab30d3650e57b5fd52748f80010e816
SabirMCoadCraftier <mahmudcse24@gmail.com>
2026-02-24 04:53:47 +0600

    Added JS

--- script.js
+++ script.js
@@ -32,3 +32,38 @@ const jobs = [
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
+    // event state ( image + text)
+  const emptyState = document.createElement("div");
+  emptyState.className = "bg-white shadow rounded-xl w-full min-h-[350px] flex flex-col items-center justify-center text-center mt-8";
+
+  const img = document.createElement("img");
+  img.src = "images/jobs.png";
+  img.className = "w-20 mb-6";
+
+  const title = document.createElement("h3");
+  title.innerText = "No jobs available";
+  title.className = "text-xl font-semibold text-gray-700 mb-2";
+
+  const subtitle = document.createElement("p");
+  subtitle.innerText = "Check back soon for new job opportunities.";
+  subtitle.className = "text-gray-500 text-sm";
+
+  emptyState.append(img, title, subtitle);  
+
+  const cardsSection = document.querySelector("section");
+  
+   function toggleEmptyState() {
+    let visible = 0;
+    jobCards.forEach(card => {
+      if(card.style.display !== "none")
+        visible++;
+    });
+
+    if(visible === 0) {
+      if(!cardsSection.contains(emptyState)) {
+        cardsSection.appendChild(emptyState);
+      }   
+    } else {
+     emptyState.remove();
+      }
+  }
